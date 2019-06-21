6e9665741ba8c4932dd77cd6c16cbffd2122dd83
jskim <kjs6030@naver.com>
2019-06-21 15:05:28 +0900

    view 추가

---
+++ app/src/main/java/com/jskim/idus/idus_codingtest/view/BindingAdapter.kt
@@ -0,0 +1,38 @@
+package com.jskim.idus.idus_codingtest.view
+
+import android.util.Log
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.jskim.idus.idus_codingtest.adapter.WeatherRecyclerAdapter
+import com.jskim.idus.idus_codingtest.model.Weather
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+object BindingAdapter {
+    @JvmStatic
+    @BindingAdapter("icon")
+    fun ImageView.loadIcon(name: String) {
+        context.let {
+            val id = it.resources.getIdentifier("ic_$name", "drawable", it.packageName)
+            Glide.with(context)
+                    .load(id)
+                    .into(this)
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter("updateItem")
+    fun RecyclerView.updateItem(list: List<Weather>?) {
+        list?.let {
+            (recyclerView.adapter as WeatherRecyclerAdapter).updateItem(list)
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter("itemDecoration")
+    fun RecyclerView.addItemDecoration(orientation: Int) {
+        addItemDecoration(DividerItemDecoration(context, orientation))
+    }
+}

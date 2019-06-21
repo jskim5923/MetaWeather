16d360eebabf0e94876427a6ac8994b62db991dc
jskim <kjs6030@naver.com>
2019-06-21 14:46:12 +0900

    Koin di 적용

---
+++ app/src/main/java/com/jskim/idus/idus_codingtest/BaseApplication.kt
@@ -0,0 +1,23 @@
+package com.jskim.idus.idus_codingtest
+
+import android.app.Application
+import com.jskim.idus.idus_codingtest.di.apiModule
+import com.jskim.idus.idus_codingtest.di.weatherModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+
+class BaseApplication:Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            if(BuildConfig.DEBUG) {
+                androidLogger()
+            }
+            androidContext(this@BaseApplication)
+            modules(listOf(apiModule, weatherModule))
+        }
+    }
+}

0244936b62c97efee4006e4e7f18df6e048b596e
jskim <kjs6030@naver.com>
2019-06-21 14:57:57 +0900

    MainActivity 구현

--- app/src/main/java/com/jskim/idus/idus_codingtest/MainActivity.kt
+++ app/src/main/java/com/jskim/idus/idus_codingtest/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.jskim.idus.idus_codingtest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.movie_search.base.BaseActivity
+import com.jskim.idus.idus_codingtest.adapter.WeatherRecyclerAdapter
+import com.jskim.idus.idus_codingtest.databinding.ActivityMainBinding
+import com.jskim.idus.idus_codingtest.model.ConsolidatedWeather
+import com.jskim.idus.idus_codingtest.model.Weather
+import com.jskim.idus.idus_codingtest.viewmodel.WeatherViewModel
+import com.jskim.idus.idus_codingtest.viewmodel.WeatherViewModelFactory
+import org.koin.android.ext.android.inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override val layoutResourceId: Int
+        get() = R.layout.activity_main
+
+    private lateinit var viewModel: WeatherViewModel
+
+    private val weatherViewModelFactory: WeatherViewModelFactory by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        init()
+        viewModel.getWeatherList()
+
+    }
+
+    private fun init() {
+        viewModel = ViewModelProviders.of(this, weatherViewModelFactory).get(WeatherViewModel::class.java)
+        dataBinding.run {
+            lifecycleOwner = this@MainActivity
+            this.viewModel = this@MainActivity.viewModel
+            dataBinding.recyclerView.apply {
+                adapter = WeatherRecyclerAdapter()
+            }
+        }
     }
 }

c90361a6897a6ab9708bdc9dc69df858c7cec580
jskim <kjs6030@naver.com>
2019-06-25 22:27:47 +0900

    recyclerView-backgroundColor/border decoration으로 변경

---
+++ app/src/main/java/com/jskim/idus/idus_codingtest/adapter/decoration/BackgroundDecoration.kt
@@ -0,0 +1,20 @@
+package com.jskim.idus.idus_codingtest.adapter.decoration
+
+import android.content.Context
+import android.graphics.Canvas
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.jskim.idus.idus_codingtest.R
+import com.jskim.idus.idus_codingtest.adapter.WeatherRecyclerAdapter
+
+class BackgroundDecoration(private val context: Context) : RecyclerView.ItemDecoration() {
+    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDraw(c, parent, state)
+        for (i in 0 until parent.childCount) {
+            val view = parent.getChildAt(i)
+            if (parent.adapter?.getItemViewType(i) == WeatherRecyclerAdapter.ItemType.TITLE.ordinal) {
+                view.setBackgroundColor(ContextCompat.getColor(context, R.color.recycler_title_background_color))
+            }
+        }
+    }
+}
--- app/src/main/java/com/jskim/idus/idus_codingtest/view/WeatherActivity.kt
+++ app/src/main/java/com/jskim/idus/idus_codingtest/view/WeatherActivity.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.ViewModelProviders
 import com.example.movie_search.base.BaseActivity
 import com.jskim.idus.idus_codingtest.R
 import com.jskim.idus.idus_codingtest.adapter.WeatherRecyclerAdapter
+import com.jskim.idus.idus_codingtest.adapter.decoration.BackgroundDecoration
+import com.jskim.idus.idus_codingtest.adapter.decoration.DividerDecoration
 import com.jskim.idus.idus_codingtest.databinding.ActivityWeatherBinding
 import com.jskim.idus.idus_codingtest.viewmodel.WeatherViewModel
 import com.jskim.idus.idus_codingtest.viewmodel.WeatherViewModelFactory
@@ -30,6 +32,8 @@ class WeatherActivity : BaseActivity<ActivityWeatherBinding>() {
             this.viewModel = this@WeatherActivity.viewModel
             dataBinding.recyclerView.apply {
                 adapter = WeatherRecyclerAdapter()
+                addItemDecoration(BackgroundDecoration(this@WeatherActivity))
+                addItemDecoration(DividerDecoration(this@WeatherActivity))
             }
         }
     }
